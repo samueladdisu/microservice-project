008043fb5d910ac9071ff63c5432c16126cfc749
samueladdisu <samueladdisu7@gmail.com>
2022-11-19 16:35:18 +0300

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=samuel
+export POSTGRES_PASSWORD=18424325
+export POSTGRES_HOST=microservice-db.cnpsosmpqs9i.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=samuel-addisu-microservice-project
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
